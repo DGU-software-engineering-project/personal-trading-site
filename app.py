cc3cef8526e8b4919cce548fb5877e4b13b3a3c6
seungy0 <51262434+seungy0@users.noreply.github.com>
2022-05-13 09:50:10 +0900

    FEAT : mongoDB 연결 후 usersDB 정상 출력 확인

--- app.py
+++ app.py
@@ -1,8 +1,29 @@
-from flask import Flask,render_template,redirect
+from flask import Flask,render_template,redirect,request
+import pymongo
+from bson.json_util import loads, dumps
 app = Flask(__name__)
+client = pymongo.MongoClient("mongodb+srv://admin:1234@cluster0.il12x.mongodb.net/tradingSiteDB?retryWrites=true&w=majority")
+db = client['tradingSiteDB']
+users = db.usersDB
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/users')
+def viewusers():
+    tmp = users.find()
+    return dumps(tmp)
+
+# @app.route('/signin',methods = ['POST', 'GET'])
+# def login():
+#     if request.method == 'POST':
+#         if request.form['profName'] in users.find():
+#             return redirect(url_for('success'))
+#         else:
+#             abort(401)
+#     else:
+#         return redirect(url_for('index'))
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
