da1dc05a40edae87c1171798127730345ebc693b
seungy0 <51262434+seungy0@users.noreply.github.com>
2022-05-24 10:35:11 +0900

    Merge branch 'flask' into main

--- appflask/__init__.py
+++ appflask/__init__.py
@@ -1,24 +1,61 @@
-#from flask import Flask, g, Response, make_response
-from flask import Flask, render_template, request, redirect, url_for, abort, session
-
+import re
+from flask import Flask,render_template,redirect,request, url_for, abort,session,flash
+import pymongo
+from bson.json_util import loads, dumps
 app = Flask(__name__)
-app.debug = True
-
-app.secret_key = 'software_engineering'
-
-
+client = pymongo.MongoClient("mongodb+srv://admin:1234@cluster0.il12x.mongodb.net/tradingSiteDB?retryWrites=true&w=majority")
+db = client['tradingSiteDB']
+users = db.usersDB
+items = db.itemsDB
+app.config.update(SECRET_KEY='secret')
 @app.route('/')
 def index():
-    return render_template('index.html')
+    entries = list(items.find())
+    return render_template('index.html', entries=entries)
 
-@app.route('/login', methods = ['GET', 'POST'])
+@app.route('/users')
+def viewusers():
+    tmp = users.find()
+    return dumps(tmp)
+
+@app.route('/items')
+def viewitems():
+    tmp = items.find()
+    return dumps(tmp)
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+@app.route('/signin',methods = ['POST', 'GET'])
 def login():
-    return render_template('login.html')
+    if request.method == 'POST':
+        id=request.form['loginid']
+        pw=request.form['loginpw']
+        userinfo = users.find_one({"ID" :id})
+        if userinfo:
+            if userinfo["PW"] == pw:
+                session['ID']=id
+                return redirect(url_for('index'))
+            else:
+                return "password incorrect"
+        else:
+            return "Id incorrect"
+    else:
+        return render_template('signin.html')
 
 @app.route('/signup', methods = ['GET', 'POST'])
 def sign_up():
-    return render_template('sign_up.html')
-        
+    if request.method == 'POST':
+        id=request.form['loginid']
+        pw=request.form['loginpw']
+        name=request.form['name']
+        userdic = {"name":name,"ID":id,"PW":pw, "FOLLOWING":[]}
+        users.insert_one(userdic)
+        flash('signup success')
+        return redirect(url_for('index'))
+    else:
+        return render_template('sign_up.html')
+
 @app.route('/item', methods = ['GET', 'POST'])
 def item():
     return render_template('item_spec.html')
@@ -36,4 +73,7 @@ def item_edit():
     return render_template('item_edit.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
+
+
