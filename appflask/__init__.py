b5ee3bb7efa7d24ade0e431c82f5fcf94bd40ce8
seungy0 <51262434+seungy0@users.noreply.github.com>
2022-05-26 00:18:33 +0900

    TEST 사진 업로드 테스크 코드

--- appflask/__init__.py
+++ appflask/__init__.py
@@ -1,13 +1,15 @@
-import re
 from flask import Flask,render_template,redirect,request, url_for, abort,session,flash
 import pymongo
 from bson.json_util import loads, dumps
+from werkzeug.utils import secure_filename
+import os
 app = Flask(__name__)
 client = pymongo.MongoClient("mongodb+srv://admin:1234@cluster0.il12x.mongodb.net/tradingSiteDB?retryWrites=true&w=majority")
 db = client['tradingSiteDB']
 users = db.usersDB
 items = db.itemsDB
 app.config.update(SECRET_KEY='secret')
+app.config['UPLOAD_FOLDER'] = './personal-trading-site/appflask/static/images'
 @app.route('/')
 def index():
     entries = list(items.find())
@@ -17,15 +19,26 @@ def index():
 def viewusers():
     tmp = users.find()
     return dumps(tmp)
-
 @app.route('/items')
 def viewitems():
     tmp = items.find()
+    return dumps(tmp)
+@app.route('/items/<userid>')
+def viewuseritems(userid):
+    tmp = items.find({"ID": userid})
     return dumps(tmp)
 @app.route('/logout')
 def logout():
     session.clear()
     return redirect(url_for('index'))
+@app.route('/file_upload', methods = ['GET', 'POST'])
+def file_upload():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(secure_filename(f.filename))
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'],f.filename))
+    return redirect(url_for('item_register'))
+
 @app.route('/signin',methods = ['POST', 'GET'])
 def login():
     if request.method == 'POST':
@@ -66,6 +79,10 @@ def mypage():
 
 @app.route('/item_register', methods = ['GET', 'POST'])
 def item_register():
+    if request.method == 'POST':
+        name=request.form['productName']
+        price=request.form['prodentPrice']
+        explain=request.form['productExplain']
     return render_template('item_register.html')
 
 @app.route('/item_edit', methods = ['GET', 'POST'])
